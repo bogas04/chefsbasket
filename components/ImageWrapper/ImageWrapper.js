d671fc2e742c7edaac7c4a27226c5ce9708d9190
Divjot Singh <bogas04@gmail.com>
2015-12-26 12:38:09 +0530

    fix default height of ImageWrapper

--- components/ImageWrapper/ImageWrapper.js
+++ components/ImageWrapper/ImageWrapper.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ImageWrapper ({ src, alt, height }) {
   return (
-    <div className="image-wrapper" style={{ height: height || '400px' }}>
+    <div className="image-wrapper" style={{ height: height || '' }}>
       <img src={src} alt={alt}/>
     </div>
   );
