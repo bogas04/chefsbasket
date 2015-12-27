a10edf780c45057dbcc522e5032a1f89362cdb9a
Divjot Singh <bogas04@gmail.com>
2015-12-27 11:00:38 +0530

    try animated route transitions

--- components/App/App.js
+++ components/App/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 import Navigation from '../Navigation';
 import Subscribe from '../Subscribe';
 import Footer from '../Footer';
+import RCTG from 'react-addons-css-transition-group';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <Navigation />
-        {this.props.children}
+        <RCTG
+          component="div"
+          transitionName="example"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+          >
+          {React.cloneElement(this.props.children, {
+            key: this.props.location.pathname
+          })}
+        </RCTG>
         <Subscribe />
         <Footer />
       </div>
